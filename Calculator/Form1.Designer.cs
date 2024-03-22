9f74d11db55cfbe59b50e1f87dafca711b0b2b83
S3RP3N7 <bigd85@hotmail.co.uk>
2024-03-22 20:58:02 +0000

    start of the button build

---
+++ Calculator/Form1.Designer.cs
@@ -0,0 +1,289 @@
+﻿namespace Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            calculatorDispaly = new Label();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            button10 = new Button();
+            button11 = new Button();
+            button12 = new Button();
+            button13 = new Button();
+            button14 = new Button();
+            button15 = new Button();
+            button16 = new Button();
+            zeroButton = new Button();
+            decimalButton = new Button();
+            equalsButton = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(448, 93);
+            button1.Name = "button1";
+            button1.Size = new Size(79, 51);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // calculatorDispaly
+            // 
+            calculatorDispaly.BackColor = SystemColors.ControlText;
+            calculatorDispaly.Font = new Font("Segoe UI", 30F, FontStyle.Regular, GraphicsUnit.Point);
+            calculatorDispaly.ForeColor = Color.White;
+            calculatorDispaly.Location = new Point(448, 9);
+            calculatorDispaly.Name = "calculatorDispaly";
+            calculatorDispaly.Size = new Size(334, 70);
+            calculatorDispaly.TabIndex = 1;
+            calculatorDispaly.Text = "0";
+            calculatorDispaly.TextAlign = ContentAlignment.MiddleRight;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(533, 93);
+            button2.Name = "button2";
+            button2.Size = new Size(79, 51);
+            button2.TabIndex = 2;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(618, 93);
+            button3.Name = "button3";
+            button3.Size = new Size(79, 51);
+            button3.TabIndex = 3;
+            button3.Text = "button3";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(703, 93);
+            button4.Name = "button4";
+            button4.Size = new Size(79, 51);
+            button4.TabIndex = 4;
+            button4.Text = "button4";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(448, 150);
+            button5.Name = "button5";
+            button5.Size = new Size(79, 51);
+            button5.TabIndex = 5;
+            button5.Text = "button5";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(533, 150);
+            button6.Name = "button6";
+            button6.Size = new Size(79, 51);
+            button6.TabIndex = 6;
+            button6.Text = "button6";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(618, 150);
+            button7.Name = "button7";
+            button7.Size = new Size(79, 51);
+            button7.TabIndex = 7;
+            button7.Text = "button7";
+            button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            button8.Location = new Point(703, 150);
+            button8.Name = "button8";
+            button8.Size = new Size(79, 51);
+            button8.TabIndex = 8;
+            button8.Text = "button8";
+            button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            button9.Location = new Point(448, 207);
+            button9.Name = "button9";
+            button9.Size = new Size(79, 51);
+            button9.TabIndex = 9;
+            button9.Text = "button9";
+            button9.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            button10.Location = new Point(535, 207);
+            button10.Name = "button10";
+            button10.Size = new Size(77, 51);
+            button10.TabIndex = 10;
+            button10.Text = "button10";
+            button10.UseVisualStyleBackColor = true;
+            // 
+            // button11
+            // 
+            button11.Location = new Point(618, 207);
+            button11.Name = "button11";
+            button11.Size = new Size(79, 51);
+            button11.TabIndex = 11;
+            button11.Text = "button11";
+            button11.UseVisualStyleBackColor = true;
+            // 
+            // button12
+            // 
+            button12.Location = new Point(703, 207);
+            button12.Name = "button12";
+            button12.Size = new Size(79, 51);
+            button12.TabIndex = 12;
+            button12.Text = "button12";
+            button12.UseVisualStyleBackColor = true;
+            // 
+            // button13
+            // 
+            button13.Location = new Point(448, 264);
+            button13.Name = "button13";
+            button13.Size = new Size(79, 51);
+            button13.TabIndex = 13;
+            button13.Text = "button13";
+            button13.UseVisualStyleBackColor = true;
+            // 
+            // button14
+            // 
+            button14.Location = new Point(535, 264);
+            button14.Name = "button14";
+            button14.Size = new Size(79, 51);
+            button14.TabIndex = 14;
+            button14.Text = "button14";
+            button14.UseVisualStyleBackColor = true;
+            // 
+            // button15
+            // 
+            button15.Location = new Point(618, 264);
+            button15.Name = "button15";
+            button15.Size = new Size(79, 51);
+            button15.TabIndex = 15;
+            button15.Text = "button15";
+            button15.UseVisualStyleBackColor = true;
+            // 
+            // button16
+            // 
+            button16.Location = new Point(703, 264);
+            button16.Name = "button16";
+            button16.Size = new Size(79, 51);
+            button16.TabIndex = 16;
+            button16.Text = "button16";
+            button16.UseVisualStyleBackColor = true;
+            // 
+            // zeroButton
+            // 
+            zeroButton.Location = new Point(448, 321);
+            zeroButton.Name = "zeroButton";
+            zeroButton.Size = new Size(164, 51);
+            zeroButton.TabIndex = 17;
+            zeroButton.Text = "0";
+            zeroButton.UseVisualStyleBackColor = true;
+            // 
+            // decimalButton
+            // 
+            decimalButton.Location = new Point(618, 321);
+            decimalButton.Name = "decimalButton";
+            decimalButton.Size = new Size(79, 51);
+            decimalButton.TabIndex = 18;
+            decimalButton.Text = ".";
+            decimalButton.UseVisualStyleBackColor = true;
+            // 
+            // equalsButton
+            // 
+            equalsButton.Location = new Point(703, 321);
+            equalsButton.Name = "equalsButton";
+            equalsButton.Size = new Size(79, 51);
+            equalsButton.TabIndex = 19;
+            equalsButton.Text = "=";
+            equalsButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(equalsButton);
+            Controls.Add(decimalButton);
+            Controls.Add(zeroButton);
+            Controls.Add(button16);
+            Controls.Add(button15);
+            Controls.Add(button14);
+            Controls.Add(button13);
+            Controls.Add(button12);
+            Controls.Add(button11);
+            Controls.Add(button10);
+            Controls.Add(button9);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(calculatorDispaly);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button button1;
+        private Label calculatorDispaly;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button10;
+        private Button button11;
+        private Button button12;
+        private Button button13;
+        private Button button14;
+        private Button button15;
+        private Button button16;
+        private Button zeroButton;
+        private Button decimalButton;
+        private Button equalsButton;
+    }
+}
